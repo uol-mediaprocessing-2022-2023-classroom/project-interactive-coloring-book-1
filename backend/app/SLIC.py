86d62247ebda18de3b1bdbd3ca38d8b546324df1
Jannessss <82639638+Jannessss@users.noreply.github.com>
2022-12-17 18:36:55 +0100

    überarbeitet

--- backend/app/SLIC.py
+++ backend/app/SLIC.py
@@ -21,21 +21,24 @@ img_segments = slic(img,
 						n_segments=500,
 						compactness=10)
 
-plt.subplot(1, 3, 1)
+plt.subplot(2, 2, 1)
 
 # Plotting the original image
 plt.imshow(img)
 
 # Detecting boundaries for labels
-plt.subplot(1, 3, 2)
+plt.subplot(2, 2, 2)
 
 # Plotting the output of marked_boundaries
 # function i.e. the image with segmented boundaries
 plt.imshow(mark_boundaries(img, img_segments))
 
-plt.subplot(1, 3, 3)
+plt.subplot(2, 2, 3)
 
 #Converts a label image into and RGB color image for visualizing the labeled regions
 plt.imshow(label2rgb(img_segments, img, kind = 'avg'))
+
+plt.subplot(2, 2, 4)
+plt.imshow(mark_boundaries(label2rgb(img_segments, img, kind = 'avg'), img_segments))
 
 plt.show()
